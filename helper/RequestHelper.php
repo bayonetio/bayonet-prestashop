7250fdcc661f0a4e4c4be90a463fec0e504ae44c
ilugobayo <ivan.lugo@bayonet.io>
2021-03-30 17:48:49 -0700

    Creates new helpers
    Creates new helpers for API request, database setup and order utilities, this to match with the revamp

---
+++ helper/RequestHelper.php
@@ -0,0 +1,164 @@
+<?php
+/**
+* 2007-2021 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2021 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class RequestHelper
+{
+    /**
+     * Performs a consulting request to the Bayonet API
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function consulting($requestBody)
+    {
+        $consultingResponse = $this->request('sigma/consult', $requestBody, 'bayonet');
+
+        return $consultingResponse;
+    }
+
+    /**
+     * Performs a feedback historical request to the Bayonet API
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function feedbackHistorical($requestBody)
+    {
+        $historicalResponse = $this->request('sigma/feedback-historical', $requestBody, 'bayonet');
+
+        return $historicalResponse;
+    }
+
+    /**
+     * Performs an update transaction request to the Bayonet API
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function updateTransaction($requestBody)
+    {
+        $updateResponse = $this->request('sigma/update-transaction', $requestBody, 'bayonet');
+
+        return $updateResponse;
+    }
+
+    /**
+     * Performs a request to the Fingerprint API
+     * Used only to validate fingerprint API keys
+     *
+     * @param string $requestBody
+     * @return array
+     */
+    public function deviceFingerprint($requestBody)
+    {
+        $deviceFingerprintResponse = $this->request('', $requestBody, 'js');
+
+        return $deviceFingerprintResponse;
+    }
+
+    /**
+     * Defines "whitelist/add" as the call to be executed in the request method
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function addWhitelist($requestBody)
+    {
+        $listResponse = $this->request('sigma/labels/whitelist/add', $requestBody, 'bayonet');
+
+        return $listResponse;
+    }
+
+    /**
+     * Defines "whitelist/remove" as the call to be executed in the request method
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function removeWhitelist($requestBody)
+    {
+        $listResponse = $this->request('sigma/labels/whitelist/remove', $requestBody, 'bayonet');
+
+        return $listResponse;
+    }
+
+    /**
+     * Defines "block/add" as the call to be executed in the request method
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function addBlocklist($requestBody)
+    {
+        $listResponse = $this->request('sigma/labels/block/add', $requestBody, 'bayonet');
+
+        return $listResponse;
+    }
+
+    /**
+     * Defines "block/remove" as the call to be executed in the request method
+     *
+     * @param array $requestBody
+     * @return array
+     */
+    public function removeBlocklist($requestBody)
+    {
+        $listResponse = $this->request('sigma/labels/block/remove', $requestBody, 'bayonet');
+
+        return $listResponse;
+    }
+
+    /**
+     * Performs a request to Bayonet/Device Fingerprint API with the provided
+     * request body to the provided endpoint
+     * 
+     * @param string $endpoint
+     * @param array $requestBody
+     * @param string $api
+     */
+    private function request($endpoint, $requestBody, $api)
+    {   
+        $apiVersion = Configuration::get('BAYONET_AF_API_VERSION');
+        $requestJson = json_encode($requestBody);
+        $requestUrl = strcmp($api, 'bayonet') === 0 ? 'https://api.bayonet.io/'.$apiVersion.'/'.$endpoint :
+            'https://fingerprinting.bayonet.io/v2/generate-fingerprint-token';
+
+        $ch = curl_init($requestUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $requestJson);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json'
+        ]);
+
+        $response = curl_exec($ch);
+        $responseJson = json_decode($response);
+        curl_close($ch);
+
+        return $responseJson;
+    }
+}
---
+++ model/BayonetDb.php
@@ -0,0 +1,113 @@
+<?php
+/**
+* 2007-2021 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2021 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class BayonetDb
+{
+    /**
+     * Creates the required tables for the module
+     */
+    public static function createTables()
+    {
+        $sqlQueries = array();
+        $installed = false;
+
+        $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_orders` (
+            `bayonet_id` int(11) NOT NULL AUTO_INCREMENT,
+            `cart_id` int(11) NULL,
+            `order_id` varchar(255) NOT NULL,
+            `bayonet_tracking_id` varchar(255) NOT NULL,
+            `consulting_api` tinyint(1) NULL,
+            `consulting_api_response` text NULL,
+            `feedback_api` tinyint(1) NULL,
+            `feedback_api_response` text NULL,
+            `decision` varchar(15) NOT NULL,
+            `triggered_rules` varchar(2000) NULL,
+            `executed` int(11) NOT NULL,
+            `date_added` timestamp default current_timestamp NOT NULL,
+            PRIMARY KEY  (`bayonet_id`)
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+    
+        $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_blocklist` (
+            `blocklist_id` int(11) NOT NULL AUTO_INCREMENT,
+            `customer_id` int(11) NOT NULL,
+            `email` varchar(255) NOT NULL,
+            `whitelist` tinyint(1) default 0 NOT NULL,
+            `blocklist` tinyint(1) default 0 NOT NULL,
+            `reason_code` int(11) NOT NULL,
+            `reason_message` text NULL,
+            `api_mode` tinyint(1) NOT NULL,
+            PRIMARY KEY  (`blocklist_id`)
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+
+        $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_fingerprint` (
+            `fingerprint_id` INT NOT NULL AUTO_INCREMENT,
+            `customer_id` INT NOT NULL,
+            `fingerprint_token` VARCHAR(45) NULL,
+            PRIMARY KEY (`fingerprint_id`)
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+        
+        $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_backfill` (
+            `backfill_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+            `backfill_status` tinyint(1) default 0 NOT NULL,
+            `processed_orders` int(10) unsigned NOT NULL DEFAULT 0,
+            `total_orders` int(10) unsigned NOT NULL DEFAULT 0,
+            `last_processed_order` varchar(45) NOT NULL,
+            `last_backfill_order` varchar(45) NOT NULL,
+            PRIMARY KEY (`backfill_id`)
+          ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+          
+
+        foreach ($sqlQueries as $query) {
+            if (Db::getInstance()->execute($query)) {
+                $installed = true;
+            }
+        }
+
+        return $installed;
+    }
+
+    /**
+     * Deletes the module tables from the database
+     */
+    public static function dropTables() 
+    {
+        $sqlQueries = array();
+        $dropped = false;
+        
+        $sqlQueries[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'bayonet_antifraud_orders`;';
+        $sqlQueries[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'bayonet_antifraud_blocklist`;';
+        $sqlQueries[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'bayonet_antifraud_fingerprint`;';
+        $sqlQueries[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'bayonet_antifraud_backfill`;';
+        
+        foreach ($sql as $query) {
+            if (Db::getInstance()->execute($query) !== false) {
+                $dropped = true;
+            }
+        }
+
+        return $dropped;
+    }
+}
