b8611be51449453281933b7d7474d6849cea98c8
ilugobayo <ivan.lugo@bayonet.io>
2021-03-30 17:24:45 -0700

    Update js files
    Updates the js files to match with the revamp

--- views/js/blocklist.js
+++ views/js/blocklist.js
@@ -1,46 +1,48 @@
 /**
- * 2007-2019 PrestaShop SA and Contributors
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/AFL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
- * International Registered Trademark & Property of PrestaShop SA
- */
+* 2007-2021 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2021 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
 
 $(document).ready(function(){
   $('#btn-addWhite').on('click', function(){
     email = $(this).attr('data-mail');
     idCustomer = $(this).attr('data-customer');
-    idBlockList = $(this).attr('data-id');
+    idBlocklist = $(this).attr('data-id');
     whitelist = $(this).attr('data-whitelist');
-    blacklist = $(this).attr('data-blacklist');
+    blocklist = $(this).attr('data-blocklist');
+    apiMode = $(this).attr('data-mode');
 
     $.ajax({
-      url: urlBlockList,
-      type: 'post',
+      url: urlBlocklist,
+      type: 'POST',
       data: {
         mail:email,
         customer:idCustomer,
-        id:idBlockList,
+        id:idBlocklist,
         whitelist:whitelist,
-        blacklist:blacklist,
-        mode:'addWhite'
+        blocklist:blocklist,
+        mode:'addWhite',
+        apiMode:apiMode
       },
       dataType: 'json',
       success: function(data) {
@@ -52,45 +54,55 @@ $(document).ready(function(){
   $('#btn-removeWhite').on('click', function(){
     email = $(this).attr('data-mail');
     idCustomer = $(this).attr('data-customer');
-    idBlockList = $(this).attr('data-id');
+    idBlocklist = $(this).attr('data-id');
     whitelist = $(this).attr('data-whitelist');
-    blacklist = $(this).attr('data-blacklist');
+    blocklist = $(this).attr('data-blocklist');
+    apiMode = $(this).attr('data-mode');
 
     $.ajax({
-      url: urlBlockList,
+      url: urlBlocklist,
       type: 'post',
       data: {
-        mail:email,
-        customer:idCustomer,
-        id:idBlockList,
-        whitelist:whitelist,
-        blacklist:blacklist,
-        mode:'removeWhite'
+        mail: email,
+        customer: idCustomer,
+        id: idBlocklist,
+        whitelist: whitelist,
+        blocklist: blocklist,
+        mode: 'removeWhite',
+        apiMode: apiMode
       },
       dataType: 'json',
-      success: function(data) {
+      success: function() {
         location.reload();
       },
     });
   });
 
-  $('#btn-addBlack').on('click', function(){
+  $('#btn-addBlock').on('click', function(){
     email = $(this).attr('data-mail');
     idCustomer = $(this).attr('data-customer');
-    idBlockList = $(this).attr('data-id');
+    idBlocklist = $(this).attr('data-id');
     whitelist = $(this).attr('data-whitelist');
-    blacklist = $(this).attr('data-blacklist');
+    blocklist = $(this).attr('data-blocklist');
+    apiMode = $(this).attr('data-mode');
+
+    console.log(email);
+    console.log(idCustomer);
+    console.log(idBlocklist);
+    console.log(whitelist);
+    console.log(blocklist);
 
     $.ajax({
-      url: urlBlockList,
+      url: urlBlocklist,
       type:'post',
       data: {
-        mail:email,
-        customer:idCustomer,
-        id:idBlockList,
-        whitelist:whitelist,
-        blacklist:blacklist,
-        mode:'addBlack'
+        mail: email,
+        customer: idCustomer,
+        id: idBlocklist,
+        whitelist: whitelist,
+        blocklist: blocklist,
+        mode: 'addBlock',
+        apiMode: apiMode
       },
       dataType: 'json',
       success: function(data) {
@@ -99,26 +111,28 @@ $(document).ready(function(){
     });
   });
 
-  $('#btn-removeBlack').on('click', function(){
+  $('#btn-removeBlock').on('click', function(){
     email = $(this).attr('data-mail');
     idCustomer = $(this).attr('data-customer');
-    idBlockList = $(this).attr('data-id');
+    idBlocklist = $(this).attr('data-id');
     whitelist = $(this).attr('data-whitelist');
-    blacklist = $(this).attr('data-blacklist');
+    blocklist = $(this).attr('data-blocklist');
+    apiMode = $(this).attr('data-mode');
 
     $.ajax({
-      url: urlBlockList,
+      url: urlBlocklist,
       type: 'post',
       data: {
-        mail:email,
-        customer:idCustomer,
-        id:idBlockList,
-        whitelist:whitelist,
-        blacklist:blacklist,
-        mode:'removeBlack'
+        mail: email,
+        customer: idCustomer,
+        id: idBlocklist,
+        whitelist: whitelist,
+        blocklist: blocklist,
+        mode: 'removeBlock',
+        apiMode: apiMode
       },
       dataType: 'json',
-      success: function(data) {
+      success: function() {
         location.reload();
       },
     });
--- views/js/fingerprint.js
+++ views/js/fingerprint.js
@@ -1,31 +1,30 @@
 /**
- * 2007-2019 PrestaShop SA and Contributors
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/AFL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
- * International Registered Trademark & Property of PrestaShop SA
- */
+* 2007-2021 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2021 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
 
 let fingerprint = '';
 $(document).ready(function() {
-  console.log('fingerprinto');
   $.getScript('https://cdn.bayonet.io/fingerprinting-2.0.min.js', function() {
   	initBayonet();
   });
@@ -37,7 +36,7 @@ $(document).ready(function() {
  */
 function initBayonet() {
   _bayonet.init({
-    js_key: bayonet_js_key,
+    js_key: bayonet_af_js_key,
     callback_function: "getResponse"
   });
   _bayonet.track();
@@ -51,16 +50,12 @@ function initBayonet() {
  * @param {Object} response
  */
 function getResponse(response) {
-  if(response.bayonet_fingerprint_token == '') {
-  } else {
-    console.log(response.bayonet_fingerprint_token);
+  if(response.bayonet_fingerprint_token) {
     $.ajax({
       url: urlFingerprint,
       type: 'post',
       data: { fingerprint: response.bayonet_fingerprint_token },
-      dataType: 'json',
-      success: function(data) {
-      }
+      dataType: 'json'
     });
   }
 }
