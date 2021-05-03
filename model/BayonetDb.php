9ef33019dce6b12e3cab7b68075c8aa77901afda
ilugobayo <ivan.lugo@bayonet.io>
2021-05-03 06:05:29 -0600

    Updates tables creation file

--- model/BayonetDb.php
+++ model/BayonetDb.php
@@ -31,7 +31,7 @@ class BayonetDb
      */
     public static function createTables()
     {
-        $sqlQueries = array();
+        $sqlQueries = [];
         $installed = false;
 
         $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_orders` (
@@ -44,9 +44,11 @@ class BayonetDb
             `feedback_api` tinyint(1) NULL,
             `feedback_api_response` text NULL,
             `decision` varchar(15) NOT NULL,
-            `triggered_rules` varchar(2000) NULL,
+            `triggered_rules` varchar(1000) NULL,
+            `api_mode` tinyint(1) NULL,
             `executed` int(11) NOT NULL,
-            `date_added` timestamp default current_timestamp NOT NULL,
+            `current_status` varchar(45) NULL,
+            `date_add` timestamp default current_timestamp NOT NULL,
             PRIMARY KEY  (`bayonet_id`)
         ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
     
@@ -56,29 +58,33 @@ class BayonetDb
             `email` varchar(255) NOT NULL,
             `whitelist` tinyint(1) default 0 NOT NULL,
             `blocklist` tinyint(1) default 0 NOT NULL,
-            `reason_code` int(11) NOT NULL,
-            `reason_message` text NULL,
+            `reason_code_blocklist` int(11) default NULL,
+            `reason_message_blocklist` text NULL,
+            `reason_code_whitelist` int(11) default NULL,
+            `reason_message_whitelist` text NULL,
+            `attempted_action_blocklist` varchar(45) default NULL,
+            `attempted_action_whitelist` varchar(45) default NULL,
             `api_mode` tinyint(1) NOT NULL,
             PRIMARY KEY  (`blocklist_id`)
         ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
 
         $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_fingerprint` (
-            `fingerprint_id` INT NOT NULL AUTO_INCREMENT,
-            `customer_id` INT NOT NULL,
-            `fingerprint_token` VARCHAR(45) NULL,
+            `fingerprint_id` int(11) NOT NULL AUTO_INCREMENT,
+            `customer_id` int(11) NOT NULL,
+            `fingerprint_token` varchar(45) default NULL,
+            `api_mode` int(11) NOT NULL,
             PRIMARY KEY (`fingerprint_id`)
         ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
-        
+
         $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'bayonet_antifraud_backfill` (
-            `backfill_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-            `backfill_status` tinyint(1) default 0 NOT NULL,
-            `processed_orders` int(10) unsigned NOT NULL DEFAULT 0,
-            `total_orders` int(10) unsigned NOT NULL DEFAULT 0,
+            `backfill_id` int(11) NOT NULL AUTO_INCREMENT,
+            `backfill_status` int(11) default 0 NOT NULL,
+            `processed_orders` int(11) default 0 NOT NULL,
+            `total_orders` int(11) default 0 NOT NULL,
             `last_processed_order` varchar(45) NOT NULL,
             `last_backfill_order` varchar(45) NOT NULL,
             PRIMARY KEY (`backfill_id`)
-          ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
-          
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
 
         foreach ($sqlQueries as $query) {
             if (Db::getInstance()->execute($query)) {
@@ -94,7 +100,7 @@ class BayonetDb
      */
     public static function dropTables() 
     {
-        $sqlQueries = array();
+        $sqlQueries = [];
         $dropped = false;
         
         $sqlQueries[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'bayonet_antifraud_orders`;';
